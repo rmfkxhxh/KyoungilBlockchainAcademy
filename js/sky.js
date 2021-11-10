0c9543608649eb713f06fb18f784a7d8e06648c5
sky <kiyoungsuh@naver.com>
2021-11-10 12:59:02 +0900

    commit on 'sky'

---
+++ js/sky.js
@@ -0,0 +1,17 @@
+function visible(){
+    let tmp = document.getElementById("login");
+
+    if (tmp.style.display == "block") {
+        tmp.style.display = "none";
+    }
+    else tmp.style.display = "block";
+}
+
+// function invisible(){
+//     if (document.getElementById("login").style.display=="block")
+//     document.getElementById("login").style.display="none";
+// }
+
+// function checkLoginInfo(id, pw){
+//     if id
+// }
