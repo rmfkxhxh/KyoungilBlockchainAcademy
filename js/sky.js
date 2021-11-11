27b90ebc334dd3c131ff526551f33392206a5a2a
sky <kiyoungsuh@naver.com>
2021-11-11 16:42:42 +0900

    commit on 'sky'

--- js/sky.js
+++ js/sky.js
@@ -13,6 +13,12 @@ function invisible(e){
     document.getElementById("login").style.display="none";
 }
 
-// function checkLoginInfo(id, pw){
-//     if id
-// }
+function checkLoginInfo(id, pw){
+    //자동로그인을 원하는지 체크
+    //database에 id가 있는경우
+    //id, pw가 일치할때
+    alert("location.href")
+    //id, pw가 일치x
+    alert("잘못된 정보입니다.")
+    //database에 id가 없는 경우
+}
